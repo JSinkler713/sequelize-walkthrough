b7e023a104a605273af2c2a8d11c13d09cd4e690
James Sinkler <jamessinkler713@gmail.com>
2021-02-04 15:35:56 -0800

    update with index.js with server code

--- dbplayground.js
+++ dbplayground.js
@@ -35,23 +35,23 @@ email: DataTypes.STRING
 //     // in this callback we have access to the user
 // })
 
-db.user.findOne({
-    where: {firstName: 'Nick'}
-  }).then(function(user) {
-      // the .get() method will clean it up to be one object
-      console.log(user.get())
-    // user will be an instance of User, remember Classes? This stores the content of the table entry with rid 2. if such an entry is not defined you will get null
-  })
-  .catch(err=> { 
-      // at the end to catch any errors that happened in the chain
-      console.log(err)
-  })
+// db.user.findOne({
+//     where: {firstName: 'Nick'}
+//   }).then(function(user) {
+//       // the .get() method will clean it up to be one object
+//       console.log(user.get())
+//     // user will be an instance of User, remember Classes? This stores the content of the table entry with rid 2. if such an entry is not defined you will get null
+//   })
+//   .catch(err=> { 
+//       // at the end to catch any errors that happened in the chain
+//       console.log(err)
+//   })
 
 
-db.user.findAll()
-.then((users)=> {
-    // print each name
-    users.forEach( user=> {
-        console.log(user.firstName + ' ' + user.lastName)
-    })
-})
+// db.user.findAll()
+// .then((users)=> {
+//     // print each name
+//     users.forEach( user=> {
+//         console.log(user.firstName + ' ' + user.lastName)
+//     })
+// })
